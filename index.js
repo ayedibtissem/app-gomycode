2e79da3dfe6befecc28e56223ecd4d09a630751f
ayedibtissem <116677898+ayedibtissem@users.noreply.github.com>
2023-03-30 00:26:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-monggoose.set("strictQuery","true")
+mongoose.set("strictQuery","true")
 app.use(express.json());
 app.use(cors());
 
